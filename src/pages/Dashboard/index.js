9dfbb648f811df82209acc34edea4dd209cadfbc
Shaan057 <roshansp639@gmail.com>
2024-03-04 02:14:33 +0530

    Misc-dashboard imported

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,11 +1,12 @@
 import Layout from "../../components/Layout/Layout"
 import LayoutProvider from "../../components/Layout/LayoutProvider"
+import MiscDashboard from "../misc";
 
 function Dashboard(){
 return(
     <LayoutProvider>
       <Layout>
-        
+        <MiscDashboard/>
       </Layout>
     </LayoutProvider>
 )
